2b47238a55318ea46ccdea45bfcb5be28db0dc1b
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-07-11 01:34:31 -0700

    added Deck class

---
+++ src/backEnd/Deck.java
@@ -0,0 +1,10 @@
+package backEnd;
+
+public class Deck {
+  public int numberOfCards;
+  public int numberOfWinsRemaining;
+  public Deck(int numberOfCards, int numberOfWinsRemaining) {
+    this.numberOfCards = numberOfCards;
+    this.numberOfWinsRemaining = numberOfWinsRemaining;
+  }
+}
