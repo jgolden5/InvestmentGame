287adf17f8dbe83e741c633d26e3e324efe04743
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-07-11 01:32:09 -0700

    WinningStatus enum and Card class created
    .

---
+++ src/backEnd/Card.java
@@ -0,0 +1,10 @@
+package backEnd;
+
+public class Card {
+  public WinningStatus status;
+
+  public Card(WinningStatus status) {
+    this.status = status;
+  }
+
+}
---
+++ src/backEnd/WinningStatus.java
@@ -0,0 +1,6 @@
+package backEnd;
+
+public enum WinningStatus {
+  win,
+  loss,
+}
