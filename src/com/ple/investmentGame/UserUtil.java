c327d987a7cf201eba4e0d885b52782b473d2ac9
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-07-12 01:05:35 -0700

    added UserUtil.prompt, which contains an infinite loop that always pauses to wait for user input nr

--- src/com/ple/investmentGame/UserUtil.java
+++ src/com/ple/investmentGame/UserUtil.java
@@ -1,8 +1,21 @@
 package com.ple.investmentGame;
 
+import java.util.Scanner;
+
 public class UserUtil {
   public static UserUtil make() {
     return new UserUtil();
   }
-  
+
+  public void prompt() {
+    boolean keepGoing = true;
+    while(keepGoing) {
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Type a command:");
+      String commandAsString = scanner.nextLine();
+      Command command = getCommandFromString();
+      command.execute();
+    }
+  }
+
 }
