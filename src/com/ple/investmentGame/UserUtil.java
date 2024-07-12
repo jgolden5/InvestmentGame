9cb4a042485d5ef3d53eb4364630075131bf7e18
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-07-12 01:25:27 -0700

    added commandKeymap to UserUtil class

--- src/com/ple/investmentGame/UserUtil.java
+++ src/com/ple/investmentGame/UserUtil.java
@@ -1,10 +1,23 @@
 package com.ple.investmentGame;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class UserUtil {
+
+  private HashMap<String, Command> keymap = new HashMap<>();
+
   public static UserUtil make() {
     return new UserUtil();
+  }
+
+  private UserUtil() {
+    initializeKeymap();
+  }
+
+  private void initializeKeymap() {
+    keymap.put("exit", new ExitCommand());
+
   }
 
   public void prompt() {
@@ -13,28 +26,17 @@ public class UserUtil {
       Scanner scanner = new Scanner(System.in);
       System.out.println("Type a command:");
       String commandAsString = scanner.nextLine();
-      Command command = getCommandFromString(commandAsString);
+      Command command = keymap.get(commandAsString);
       UserDestination userDestination = UserDestination.prompt;
       if(command != null) {
         userDestination = command.executeAndGetUserDestination();
+      } else {
+        System.out.println("Command not recognized");
       }
       if(userDestination == UserDestination.exit) {
         keepGoing = false;
       }
     }
-  }
-
-  private Command getCommandFromString(String commandAsString) {
-    Command command;
-    switch(commandAsString) {
-      case "exit", "q":
-        command = new ExitCommand();
-        break;
-      default:
-        System.out.println("Command " + commandAsString + " not recognized.");
-        command = null;
-    }
-    return command;
   }
 
 }
