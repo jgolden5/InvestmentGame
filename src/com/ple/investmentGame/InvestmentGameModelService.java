92cfcb189b0b7c233a3d3ec73ef8157daa16ff5d
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-07-15 00:31:10 -0700

    make InvestmentGameModelService.get/putTokens nr

--- src/com/ple/investmentGame/InvestmentGameModelService.java
+++ src/com/ple/investmentGame/InvestmentGameModelService.java
@@ -2,6 +2,7 @@ package com.ple.investmentGame;
 
 public class InvestmentGameModelService {
   public Deck deck;
+  private int tokens;
 
   public static InvestmentGameModelService make() {
     return new InvestmentGameModelService();
@@ -13,6 +14,14 @@ public class InvestmentGameModelService {
 
   public void putDeck(Deck deck) {
     this.deck = deck;
+  }
+
+  public int getTokens() {
+    return tokens;
+  }
+
+  public void putTokens(int tokens) {
+    this.tokens = tokens;
   }
 
 }
