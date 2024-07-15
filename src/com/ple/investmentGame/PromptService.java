54dde95bc8ef8f0bb7cfd5af1350152d59c20f18
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-07-15 00:01:55 -0700

    make PromptService class with empty methods nr

---
+++ src/com/ple/investmentGame/PromptService.java
@@ -0,0 +1,17 @@
+package com.ple.investmentGame;
+
+public class PromptService {
+  
+  public static PromptService make() {
+    return new PromptService();
+  }
+
+  public void generateDeck() {
+
+  }
+
+  public void invest() {
+
+  }
+
+}
