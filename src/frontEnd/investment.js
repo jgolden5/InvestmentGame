00d23cff18cd74333817cad97c151f13608cdcf2
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-10-12 20:18:07 -0700

    change percentOfWinning to winningCards, which are displayed in the game

--- src/frontEnd/investment.js
+++ src/frontEnd/investment.js
@@ -1,18 +1,21 @@
 const startingInvestmentInput = document.getElementById("startingInvestmentInput");
-const increasePerWinInput = document.getElementById("increasePerWinInput");
+const numberOfWinCardsInput = document.getElementById("numberOfWinCardsInput");
 const deckSizeInput = document.getElementById("deckSizeInput");
 
 const startButton = document.getElementById("startButton");
-startButton.addEventListener("click", () => gameStart(startingInvestmentInput.value, increasePerWinInput.value, deckSizeInput.value));
+startButton.addEventListener("click", () => gameStart(startingInvestmentInput.value, numberOfWinCardsInput.value, deckSizeInput.value));
 
-let percentIncreasePerWin, investmentRemaining, cardsRemaining;
+let winningCardsRemaining, investmentRemaining, cardsRemaining, winChance;
 
-function gameStart(startingInvestmentInput, increasePerWinInput, deckSizeInput) {
-  if(startingInvestmentInput > 0 && increasePerWinInput > 0 && deckSizeInput > 0) {
+function gameStart(startingInvestmentInput, numberOfWinCardsInput, deckSizeInput) {
+  if(startingInvestmentInput > 0 && numberOfWinCardsInput > 0 && deckSizeInput > 0) {
     investmentRemaining = startingInvestmentInput;
-    percentIncreasePerWin = increasePerWinInput;
+    winningCardsRemaining = numberOfWinCardsInput;
     cardsRemaining = deckSizeInput;
+    winChance = calcWinChanceAsPercentage(winningCardsRemaining, cardsRemaining);
+    document.getElementById("heading").innerText = winChance + " chance of winning...";
     document.getElementById("investmentRemaining").innerText = "Investment Remaining = " + investmentRemaining;
+    document.getElementById("winningCardsRemaining").innerText = "Winning Cards Remaining = " + winningCardsRemaining;
     document.getElementById("cardsRemaining").innerText = "Cards Remaining = " + cardsRemaining;
     document.getElementById("startingInputs").style = "display: none";
     document.getElementById("startButton").style = "display: none";
@@ -21,7 +24,10 @@ function gameStart(startingInvestmentInput, increasePerWinInput, deckSizeInput) 
     document.getElementById("errorMessage").innerText = "Error: Please fill in each of the fields with a number greater than 0."
     setTimeout(() => {
       document.getElementById("errorMessage").innerText = ""
-      location.reload();
     }, 1000)
   }
+}
+
+function calcWinChanceAsPercentage(winningCardsRemaining, cardsRemaining) {
+  return Math.round(winningCardsRemaining / cardsRemaining * 100 * 100) / 100 + '%';
 }
