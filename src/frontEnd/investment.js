5a68be9a79ecdc67239a54d940be3c0ab21dd27c
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-10-12 18:45:30 -0700

    3 inputs with initial values for game are set in place
    .

--- src/frontEnd/investment.js
+++ src/frontEnd/investment.js
@@ -1,10 +1,25 @@
-const startGameButton = document.getElementById("startGame")
-startGameButton.addEventListener("click", () => hideElement("hiddenP"));
-function hideElement(idOfElementToHide) {
-  const hidden = document.getElementById(idOfElementToHide)
-  if(hidden.style.display === "none") {
-    hidden.style.display = "block";
+const startingInvestmentInput = document.getElementById("startingInvestmentInput");
+const increasePerWinInput = document.getElementById("increasePerWinInput");
+const deckSizeInput = document.getElementById("deckSizeInput");
+
+const startButton = document.getElementById("startButton");
+startButton.addEventListener("click", () => gameStart(startingInvestmentInput.value, increasePerWinInput.value, deckSizeInput.value));
+
+let percentIncreasePerWin, investmentRemaining, cardsRemaining;
+
+function gameStart(startingInvestmentInput, increasePerWinInput, deckSizeInput) {
+  if(startingInvestmentInput > 0 && increasePerWinInput > 0 && deckSizeInput > 0) {
+    investmentRemaining = startingInvestmentInput;
+    percentIncreasePerWin = increasePerWinInput;
+    cardsRemaining = deckSizeInput;
+    document.getElementById("investmentRemaining").innerHTML = "Investment Remaining = " + investmentRemaining;
+    document.getElementById("cardsRemaining").innerHTML = "Cards Remaining = " + cardsRemaining;
+    document.getElementById("startingInputs").style = "display: none";
   } else {
-    hidden.style.display = "none";
+    document.getElementById("errorMessage").innerHTML = "Error: Please fill in each of the fields with a number greater than 0."
+    setTimeout(() => {
+      document.getElementById("errorMessage").innerHTML = ""
+      location.reload();
+    }, 1000)
   }
 }
